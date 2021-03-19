4cce91fbce9571972c03d3a63d2fb843fccf2133
zeykaya <1zeynep.kayaaa@gmail.com>
2021-03-19 15:16:47 +0300

    Initial commit

---
+++ app/src/main/java/com/kaya/simpsonbook/MainActivity.java
@@ -0,0 +1,42 @@
+package com.kaya.simpsonbook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView listView=findViewById(R.id.listView);
+
+        Simpson homer= new Simpson("Homer Simpson","Nuclear",R.drawable.homer);
+        Simpson lisa= new Simpson("Lisa Simpson","Student",R.drawable.lisa);
+        Simpson bart= new Simpson("Bart Simpson","Student",R.drawable.bart);
+
+        ArrayList<Simpson>SimpsonList=new ArrayList<>();
+        SimpsonList.add(homer);
+        SimpsonList.add(lisa);
+        SimpsonList.add(bart);
+
+        CustomAdapter customAdapter=new CustomAdapter(SimpsonList,MainActivity.this);
+        listView.setAdapter(customAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent=new Intent(MainActivity.this,DetailsActivity.class);
+               intent.putExtra("selectedSimpson",SimpsonList.get(position));
+                startActivity(intent);
+            }
+        });
+    }
+}
